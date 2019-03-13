56c44938e5dc493cfde8e84bd0333a8153f4b05d
C.K.Rajadurai <42277015+ck2605@users.noreply.github.com>
2019-03-13 21:16:10 +0530

    Update linear_search(2).py

--- searching/linear_search(2).py
+++ searching/linear_search(2).py
@@ -1,18 +1,21 @@
-from array import *
-arr = array('i', [])
-n = int(input("enter the no of elements:"))
+# Searching Algorithm
+# LINEAR SEARCH
+# given an array of elements not in the ordered manner,search a given KEY is present or not....
+from array import *                                          # importing array func from lib
+arr = array('i', [])                                         # declaring an array
+flag = 0
+n = int(input("enter the no of elements:"))                  # Getting array length from the user
 for i in range(n):
-    x = int(input("enter the elements:"))
+    x = int(input("enter the elements:"))                    #Getting the elements from the user
     arr.append(x)
 print(arr)
 val = int(input("enter the value to search:"))
-k = 0
 for e in arr:
     if e == val:
-        print(k)
+        flag = 1
         break
-    k = k+1
-
-print(arr.index(val))
-
+if flag == 1:   
+    print("the element is found at :",arr.index(val))         # printing the position of the element 
+else:
+    print("the elements is not found")
 
