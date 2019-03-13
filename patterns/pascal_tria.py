479223c35c2d52b069015649a0e383991ba8e073
C.K.Rajadurai <42277015+ck2605@users.noreply.github.com>
2019-03-13 22:30:06 +0530

    Update pascal_tria.py

--- patterns/pascal_tria.py
+++ patterns/pascal_tria.py
@@ -1,3 +1,20 @@
+"""
+@author : CK =>ckrajadurai7@gmail.com
+@github : https://github.com/ck2605
+@date : 13/03/2019
+
+
+=================================PASCAL TRIANGLE=====================================
+if no of row = 5....
+ANSWER(o/p):
+    1
+   212
+  32123
+ 4321234
+543212345
+
+"""
+
 n = int(input("enter the no of rows:"))
 for i in range(1, n + 1):
     for j in range(1, n - i + 1):
