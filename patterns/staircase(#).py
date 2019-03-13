bb6c6d4a8fcf59ce60df1e7493b0f534b1f01868
C.K.Rajadurai <42277015+ck2605@users.noreply.github.com>
2019-03-13 22:35:21 +0530

    Update staircase(#).py

--- patterns/staircase(#).py
+++ patterns/staircase(#).py
@@ -1,3 +1,18 @@
+"""
+@author : CK =>ckrajadurai7@gmail.com
+@github : https://github.com/ck2605
+@date : 13/03/2019
+
+==============================STAIRCASE(#) PATTERN=====================================================================
+IF ROW = 5
+ANSWER(O/P):
+# 
+# # 
+# # # 
+# # # # 
+# # # # # 
+"""
+
 n = int(input("enter the row limit:"))
 for i in range(n+1):
     for j in range(i):
